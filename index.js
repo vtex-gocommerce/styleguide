3cb029b687d5dfc020db2b4890eb0b51e0b1834a
João Pedro Gonçalves <joao.kamun@gmail.com>
2018-02-26 15:12:08 -0300

    Update

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-import IconAdd from './src/icons/IconAdd'
-import IconCalendar from './src/icons/IconCalendar'
-import IconCaret from './src/icons/IconCaret'
-import IconCart from './src/icons/IconCart'
-import IconChartBar from './src/icons/IconChartBar'
-import IconClose from './src/icons/IconClose'
-import IconCloseAlt from './src/icons/IconCloseAlt'
-import IconExchange from './src/icons/IconExchange'
-import IconFilter from './src/icons/IconFilter'
-import IconGoogle from './src/icons/IconGoogle'
-import IconInbox from './src/icons/IconInbox'
-import IconMoney from './src/icons/IconMoney'
-import IconMore from './src/icons/IconMore'
-import IconQuestion from './src/icons/IconQuestion'
-import IconTrash from './src/icons/IconTrash'
-import IconVisible from './src/icons/IconVisible'
-import Alert from './src/components/Alert'
-import AuthButton from './src/components/AuthButton'
-import Button from './src/components/Button'
-import Tag from './src/components/Tag'
+'use strict'
 
-exports.module = {
-  IconAdd,
-  IconCalendar,
-  IconCaret,
-  IconCart,
-  IconChartBar,
-  IconClose,
-  IconCloseAlt,
-  IconExchange,
-  IconFilter,
-  IconGoogle,
-  IconInbox,
-  IconMoney,
-  IconMore,
-  IconQuestion,
-  IconTrash,
-  IconVisible,
-  Alert,
-  AuthButton,
-  Button,
-  Tag,
-}
+var _Lib = require('./lib')
+
+var _Lib2 = _interopRequireDefault(_Lib)
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+export default _Lib2
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import IconAdd from './src/icons/IconAdd'
+import IconCalendar from './src/icons/IconCalendar'
+import IconCaret from './src/icons/IconCaret'
+import IconCart from './src/icons/IconCart'
+import IconChartBar from './src/icons/IconChartBar'
+import IconClose from './src/icons/IconClose'
+import IconCloseAlt from './src/icons/IconCloseAlt'
+import IconExchange from './src/icons/IconExchange'
+import IconFilter from './src/icons/IconFilter'
+import IconGoogle from './src/icons/IconGoogle'
+import IconInbox from './src/icons/IconInbox'
+import IconMoney from './src/icons/IconMoney'
+import IconMore from './src/icons/IconMore'
+import IconQuestion from './src/icons/IconQuestion'
+import IconTrash from './src/icons/IconTrash'
+import IconVisible from './src/icons/IconVisible'
+import Alert from './src/components/Alert'
+import AuthButton from './src/components/AuthButton'
+import Button from './src/components/Button'
+import Tag from './src/components/Tag'
+
+exports.module = {
+  IconAdd,
+  IconCalendar,
+  IconCaret,
+  IconCart,
+  IconChartBar,
+  IconClose,
+  IconCloseAlt,
+  IconExchange,
+  IconFilter,
+  IconGoogle,
+  IconInbox,
+  IconMoney,
+  IconMore,
+  IconQuestion,
+  IconTrash,
+  IconVisible,
+  Alert,
+  AuthButton,
+  Button,
+  Tag,
+}
