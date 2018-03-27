4f4839513d8b7955149c0e0235075ea999275b2d
matheus costa <mf.costa@live.com>
2018-03-27 13:32:19 -0300

    adds forcePage in pagination component

--- src/lib/components/Navigation/Pagination/README.md
+++ src/lib/components/Navigation/Pagination/README.md
@@ -1,10 +1,45 @@
 ```js
-<div>
-    <div className="flex justify-center mb5">
-        <Pagination initialPage={1} pageCount={600} onPageChange={(data) => console.log('Page changed!')} />
-    </div>
-    <div className="flex justify-center">
-        <Pagination initialPage={15} pageCount={30} isCompact />
-    </div>
+class WithForcePage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { value: 0 }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(value) {
+    this.setState({ value })
+  }
+
+  render() {
+    const number = (Math.random(1, 15) * 100).toFixed(0)
+    return (
+      <div>
+        {'with forcePage'}
+        <div className="flex justify-center mb5">
+          <Pagination
+            forcePage={parseInt(this.state.value)}
+            pageCount={100}
+            onPageChange={data => console.log('Page changed!', data)}
+            isCompact
+          />
+        </div>
+        <div className="flex justify-center mb5">
+          <Button onClick={() => this.handleChange(number)}>Next value: {parseFloat(number) + 1}</Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+;<div>
+  <div className="flex justify-center mb5">
+    <Pagination initialPage={1} pageCount={600} onPageChange={data => console.log('Page changed!')} />
+  </div>
+  <div className="flex justify-center">
+    <Pagination initialPage={15} pageCount={30} isCompact />
+  </div>
+  <hr />
+  <WithForcePage />
 </div>
 ```
--- src/lib/components/Navigation/Pagination/index.js
+++ src/lib/components/Navigation/Pagination/index.js
@@ -5,41 +5,46 @@ import IconAngle from '../../../icons/IconAngle'
 import styles from './style.css'
 
 class Pagination extends PureComponent {
-    handleChange = (data) => {
-        this.props.onPageChange(data)
-    }
+  handleChange = data => {
+    this.props.onPageChange(data)
+  }
 
-    render() {
-        const { initialPage, pageCount, isCompact } = this.props
+  render() {
+    const { initialPage, pageCount, isCompact, forcePage } = this.props
 
-        return (
-            <Paginate previousLabel={<IconAngle side="left" />}
-                nextLabel={<IconAngle side="right" />}
-                initialPage={initialPage}
-                pageCount={pageCount}
-                onPageChange={this.handleChange}
-                marginPagesDisplayed={isCompact ? 1 : 2}
-                pageRangeDisplayed={isCompact ? 2 : 5}
-                containerClassName={styles.pagination}
-                activeClassName={styles.active}
-                breakClassName={styles.break} />
-        )
-    }
+    return (
+      <Paginate
+        previousLabel={<IconAngle side="left" />}
+        nextLabel={<IconAngle side="right" />}
+        initialPage={initialPage}
+        pageCount={pageCount}
+        onPageChange={this.handleChange}
+        marginPagesDisplayed={isCompact ? 1 : 2}
+        pageRangeDisplayed={isCompact ? 2 : 5}
+        containerClassName={styles.pagination}
+        activeClassName={styles.active}
+        breakClassName={styles.break}
+        forcePage={forcePage}
+      />
+    )
+  }
 }
 
 Pagination.propTypes = {
-    /** Define selected page. */
-    initialPage: PropTypes.number.isRequired,
-    /** Define total count of pages. */
-    pageCount: PropTypes.number.isRequired,
-    /** Function to run when page is changed. */
-    onPageChange: PropTypes.func,
-    /** Set pagination to compact mode. */
-    isCompact: PropTypes.bool
+  /** Define selected page. */
+  initialPage: PropTypes.number.isRequired,
+  /** Define total count of pages. */
+  pageCount: PropTypes.number.isRequired,
+  /** Function to run when page is changed. */
+  onPageChange: PropTypes.func,
+  /** Set pagination to compact mode. */
+  isCompact: PropTypes.bool,
+  /**  To override selected page with parent prop. */
+  forcePage: PropTypes.number
 }
 
 Pagination.defaultProps = {
-    onPageChange: () => { }
+  onPageChange: () => {}
 }
 
 export default Pagination
