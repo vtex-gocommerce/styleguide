9866018ba32010bbec1f797ee70e7bbe2b27d5c5
matheus costa <mf.costa@live.com>
2018-05-03 11:26:56 -0300

    fix placeholder structure

--- src/lib/components/General/Placeholder/index.js
+++ src/lib/components/General/Placeholder/index.js
@@ -1,23 +1,51 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './styles.css'
 
-class Placeholder extends PureComponent {
+class Placeholder extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ready: false,
+      loading: false
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps)
+    if (!this.state.ready && !nextProps.isPlaceholderActive) {
+      console.log(nextProps)
+      this.setState(prevState => ({
+        ...prevState,
+        loading: true
+      }))
+
+      setTimeout(
+        () =>
+          this.setState(prevState => ({
+            ready: true,
+            loading: false
+          })),
+        500
+      )
+    }
+  }
+
   render() {
-    return (
-      <div>
-        {this.props.isPlaceholderActive ? (
-          <div className={`animated-background ${this.props.className}`} />
-        ) : (
-          <React.Fragment>
-            <div className={`fadeOut ${this.props.className}`}>
-              <div className={`animated-background ${this.props.className}`} />
-            </div>
-            <div className="fadeIn">{this.props.children()}</div>
-          </React.Fragment>
-        )}
-      </div>
-    )
+    const { ready, loading } = this.state
+    console.log(ready, loading)
+    if (!ready && !loading) return <div className={`animated-background ${this.props.className}`} />
+    if (!ready && loading)
+      return (
+        <div className={` ${this.props.className}`}>
+          <div className=" fadeOut">
+            <div className={` animated-background `} />
+          </div>
+        </div>
+      )
+    if (ready && !loading) return <div className="fadeIn">{this.props.children()}</div>
+
+    return <span />
   }
 }
 //
