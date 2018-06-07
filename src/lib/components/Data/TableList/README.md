95e6b08f8c0531d1104e4da66461a61a691a7164
Renato S Moreno <renatomorenocz@gmail.com>
2018-06-07 16:53:49 -0300

    Adds Tablelist

---
+++ src/lib/components/Data/TableList/README.md
@@ -0,0 +1,88 @@
+```js
+const columns = [
+  {
+    id: 'Name',
+    label: 'Name'
+  },
+  {
+    id: 'Address',
+    label: 'Address'
+  },
+  {
+    id: 'Price',
+    label: <IconMoney />,
+    isCentered: true,
+    size: 15,
+    textAlign: 'center'
+  },
+  {
+    id: 'Count',
+    label: 'Count',
+    size: 10,
+    textAlign: 'center'
+  },
+  {
+    id: 'Active',
+    label: 'Active',
+    textAlign: 'right'
+  }
+]
+
+const rows = [
+  {
+    id: 1,
+    Name: 'Example Test',
+    Address: 'Street of Test',
+    Count: 245,
+    Price: (
+      <div>
+        <span className="navy-60 strike">$ 2399.00</span>
+        <br />$ 2199.90
+      </div>
+    ),
+    Active: 'Active'
+  },
+  {
+    id: 3,
+    Name: 'Last Test',
+    Address: 'Just a big street, also to see what will happen',
+    Count: (
+      <p className="yellow">
+        24 <IconWarning className="g-ml1" family="regular" />
+      </p>
+    ),
+    Active: 'Inactive'
+  }
+]
+
+class ShowModal extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { placeholderIsActive: true }
+    this.tooglePlaceHolderStatus = this.tooglePlaceHolderStatus.bind(this)
+  }
+
+  tooglePlaceHolderStatus(value) {
+    this.setState({ placeholderIsActive: !this.state.placeholderIsActive })
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.tooglePlaceHolderStatus}> Change status </Button> Is Placeholder Active:{' '}
+        {this.state.placeholderIsActive ? 'active' : 'inactive'}
+        <div>
+          <div className="g-mb4 g-mt4">
+            <TableList columns={columns} rows={rows} isLoading={this.state.placeholderIsActive} />
+          </div>
+          <div className="g-mb4 g-mt4">
+            <TableList columns={columns} rows={rows} selectable onChange={e => console.log(e)} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+;<ShowModal />
+```
---
+++ src/lib/components/Data/TableList/index.js
@@ -0,0 +1,99 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import style from './style.css'
+import Placeholder from './../../General/Placeholder'
+
+const textAligns = {
+  right: 'tr',
+  left: 'tl',
+  center: 'tc'
+}
+
+class TableList extends PureComponent {
+  render() {
+    const { columns, rows } = this.props
+    const lastColumn = columns.length - 1
+
+    return (
+      <table className={`w-100`} cellSpacing="0">
+        <thead>
+          <tr>
+            {columns.map((column, index) => {
+              const textAlign = column.textAlign || 'left'
+              return (
+                <th
+                  key={column.id}
+                  className={`${index === 0 ? 'g-pl0' : 'g-pl4'} ${index === lastColumn ? 'g-pr0' : 'g-pr4'} 
+                  ${textAligns[textAlign]} c-on-base-2 fw4 `}
+                  style={{ width: column.size + '%' }}
+                >
+                  {column.label}
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody className="bg-base-1">
+          {this.props.isLoading
+            ? [...Array(3).keys()].map(e => (
+                <tr key={e}>
+                  {[...Array(this.props.columns.length).keys()].map(e => (
+                    <td key={e} className={`g-pv5 g-ph4 c-on-base-1 tc `}>
+                      <Placeholder className="g-h2 w-100 g-mt2 br4" isPlaceholderActive={this.props.isLoading} />
+                    </td>
+                  ))}
+                </tr>
+              ))
+            : rows.map((fields, index) => {
+                const formatted_row = columns.map((column, indexColumn) => {
+                  const textAlign = column.textAlign || 'left'
+                  return (
+                    <td
+                      key={index + column.id}
+                      className={`${indexColumn === 0 ? 'g-pl0' : 'g-pl4'} ${
+                        indexColumn === lastColumn ? 'g-pr0' : 'g-pr4'
+                      }
+                      g-pv2 c-on-base-1 ${textAligns[textAlign]}`}
+                    >
+                      <Placeholder className="g-h2 w-100 g-mt2 br4" isPlaceholderActive={false}>
+                        {() => fields[column.id]}
+                      </Placeholder>
+                    </td>
+                  )
+                })
+
+                return <tr key={index}>{formatted_row}</tr>
+              })}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+TableList.propTypes = {
+  /** Columns that will have on table. */
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      /** ID of the column. */
+      id: PropTypes.string.isRequired,
+      /** Label of the column. */
+      label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+      /** Size of the column in percentage. */
+      size: PropTypes.number,
+      /** Make the column text align. */
+      textAlign: PropTypes.oneOf(['left', 'right', 'center'])
+    })
+  ).isRequired,
+  /** Rows that will be show on table. */
+  rows: PropTypes.array.isRequired,
+  /** Is table in Loading State */
+  isLoading: PropTypes.bool,
+  onChange: PropTypes.func
+}
+
+TableList.defaultProps = {
+  isLoading: false,
+  onChange: () => {}
+}
+
+export default TableList
