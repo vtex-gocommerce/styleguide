f0da1e4ac5f6c6d747efc69f3a67dc545bf21c14
Johny Velho <johny.velho@live.com>
2018-06-12 14:39:57 -0300

    TextLimitCounter

---
+++ src/lib/components/Indicators/TextLimitCounter/index.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import styles from './style.css'
+
+class TextLimitCounter extends PureComponent {
+  render() {
+    const { text, limit, className } = this.props
+
+    return (
+      <span className={`c-on-base-2 o-50 ${className}`}>
+        {text.length} of {limit}
+      </span>
+    )
+  }
+}
+
+TextLimitCounter.propTypes = {
+  text: PropTypes.string,
+  limit: PropTypes.number,
+  className: PropTypes.string
+}
+
+TextLimitCounter.defaultProps = {
+  className: ''
+}
+
+export default TextLimitCounter
