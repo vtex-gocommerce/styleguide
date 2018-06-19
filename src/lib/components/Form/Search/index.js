7a75469a42c0493b1104bc967b46efd643bea2a0
Renato S Moreno <renatomorenocz@gmail.com>
2018-06-19 14:29:58 -0300

    Update tachyons

--- src/lib/components/Form/Search/index.js
+++ src/lib/components/Form/Search/index.js
@@ -35,7 +35,7 @@ class Search extends PureComponent {
   render() {
     const { placeholder, withOptions, options, name, size, nav } = this.props
 
-    const classes = !nav ? "b--base-4" : "bg-base-inverted-2-25 bn"
+    const classes = !nav ? "b--base-4" : "bg-base-inverted-5 bn"
     const classesIcon = !nav ? "c-on-base-2" : "c-base-inverted-3"
 
     return (
