35c644b3c8c57eb310f709fff0c0ac88962c2d15
Johny Velho <johny.velho@live.com>
2018-06-27 10:35:45 -0300

    Tag component with styles primary, danger, warning and success

--- src/lib/components/Indicators/Tag/README.md
+++ src/lib/components/Indicators/Tag/README.md
@@ -6,5 +6,25 @@
   <div className="dib">
     <Tag onRemove={() => console.log('Tag removed!')}>Tag</Tag>
   </div>
+  <div className="dib">
+    <Tag style="primary" onRemove={() => console.log('Tag removed!')}>
+      Tag
+    </Tag>
+  </div>
+  <div className="dib">
+    <Tag style="danger" onRemove={() => console.log('Tag removed!')}>
+      Tag
+    </Tag>
+  </div>
+  <div className="dib">
+    <Tag style="warning" onRemove={() => console.log('Tag removed!')}>
+      Tag
+    </Tag>
+  </div>
+  <div className="dib">
+    <Tag style="success" onRemove={() => console.log('Tag removed!')}>
+      Tag
+    </Tag>
+  </div>
 </div>
 ```
--- src/lib/components/Indicators/Tag/index.js
+++ src/lib/components/Indicators/Tag/index.js
@@ -3,18 +3,34 @@ import PropTypes from 'prop-types'
 import styles from './style.css'
 import IconClose from '../../../icons/IconClose'
 
+const styleClasses = {
+  default: 'bg-white b--on-base-2',
+  primary: 'bg-primary-light b--primary',
+  danger: 'bg-danger-light b--danger',
+  warning: 'bg-warning-light b--warning',
+  success: 'bg-success-light b--success'
+}
+
 class Tag extends PureComponent {
   render() {
-    const { onRemove } = this.props
+    const { onRemove, style } = this.props
 
-    let classes = `${styles.gc_tag} g-f2 g-t5 dib g-ph4 g-mr1 g-h10 ba br2 c-on-base-2 b--on-base-2 b--dashed flex justify-center items-center `
+    let tagStyleClasses = styleClasses[style]
+
+    let classes = `${
+      styles.gc_tag
+    } ${tagStyleClasses} g-f2 g-t5 dib g-ph4 g-mr1 g-h10 ba br2 c-on-base-2 b--dashed flex justify-center items-center `
     if (onRemove) classes += 'hover-bg-base-2 pointer hover-bg-base-3 hover-b--base-3'
 
     return (
-      <span className={classes} onClick={onRemove}>
+      <span className={classes}>
         <span className="flex items-center">
           {this.props.children}
-          {onRemove && <IconClose className="g-ml2" family="regular" height={18} width={18} />}
+          {onRemove && (
+            <span onClick={onRemove}>
+              <IconClose className="g-ml2" family="regular" height="18px" width="18px" />
+            </span>
+          )}
         </span>
       </span>
     )
@@ -24,11 +40,13 @@ class Tag extends PureComponent {
 Tag.propTypes = {
   /** Function that will be called when user click to remove tag. */
   onRemove: PropTypes.func,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  style: PropTypes.oneOf(['default', 'primary', 'danger', 'warning', 'success'])
 }
 
 Tag.defaultProps = {
-  onRemove: null
+  onRemove: null,
+  style: 'default'
 }
 
 export default Tag
