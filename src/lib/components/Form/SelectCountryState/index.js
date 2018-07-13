0f7ba61d4ac19cea1c5a7c2e10d79dc13ade83be
matheus costa <mf.costa@live.com>
2018-07-13 15:32:11 -0300

    fix  SelectCountryState load file

--- src/lib/components/Form/SelectCountryState/index.js
+++ src/lib/components/Form/SelectCountryState/index.js
@@ -23,7 +23,7 @@ class SelectCountryStates extends PureComponent {
   }
 
   componentWillReceiveProps = nextProps => {
-    if (this.props.countryCode != nextProps.countryCode) this.setLocale(nextPropscountryCode)
+    if (this.props.countryCode != nextProps.countryCode) this.setLocale(nextProps.countryCode)
     if (this.state.value != nextProps.value) this.setState(prevState => ({ ...prevState, value: nextProps.value }))
   }
 
