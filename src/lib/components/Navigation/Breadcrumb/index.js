8ab43f48b0d62b698b8732f1f4552be09e3b836f
Johny Velho <johny.velho@live.com>
2018-07-24 14:06:50 -0300

    Breadcrumb fix proptypes

--- src/lib/components/Navigation/Breadcrumb/index.js
+++ src/lib/components/Navigation/Breadcrumb/index.js
@@ -3,32 +3,40 @@ import PropTypes from 'prop-types'
 import IconArrow from '../../../icons/IconArrow'
 
 class Breadcrumb extends PureComponent {
-
   renderItemLink = (item, icon = false) => {
-    const classesLink = "c-on-base-2 inline-flex items-center no-underline hover-c-primary"
+    const classesLink = 'c-on-base-2 inline-flex items-center no-underline hover-c-primary'
 
     if (!this.props.link)
-      return <React.Fragment>
-        <a href={item.to} className={classesLink}>{icon && <IconArrow side="left" className="g-mr2" />} {item.title}</a>
-      </React.Fragment>
+      return (
+        <React.Fragment>
+          <a href={item.to} className={classesLink}>
+            {icon && <IconArrow side="left" className="g-mr2" />} {item.title}
+          </a>
+        </React.Fragment>
+      )
 
     const Link = this.props.link
-    return <React.Fragment>
-      <Link to={item.to} className={classesLink}>{icon && <IconArrow side="left" className="g-mr2" />} {item.title}</Link>
-    </React.Fragment>
+    return (
+      <React.Fragment>
+        <Link to={item.to} className={classesLink}>
+          {icon && <IconArrow side="left" className="g-mr2" />} {item.title}
+        </Link>
+      </React.Fragment>
+    )
   }
 
-  renderItemText = (item) => {
+  renderItemText = item => {
     return item.title
   }
 
   renderItem = (item, index) => {
-    return (<React.Fragment>
-      {item.hasOwnProperty('to') ? this.renderItemLink(item, index === 0) : this.renderItemText(item)}
-      <span className="c-on-base-2"> {this.props.separator} </span>
-    </React.Fragment>)
+    return (
+      <React.Fragment>
+        {item.hasOwnProperty('to') ? this.renderItemLink(item, index === 0) : this.renderItemText(item)}
+        <span className="c-on-base-2"> {this.props.separator} </span>
+      </React.Fragment>
+    )
   }
-
 
   render() {
     const { items } = this.props
@@ -50,8 +58,8 @@ Breadcrumb.propTypes = {
   /** List of items. */
   items: PropTypes.arrayOf(
     PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      to: PropTypes.string
+      title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+      to: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
     })
   ).isRequired,
   /** Separator character */
