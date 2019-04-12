aa126f51be86762feb22eefc5b0aeee276ed1fa7
Augusto Lazaro <aemlazaro@gmail.com>
2019-04-12 15:51:45 -0300

    Use link

--- react/components/General/EmptyContent/README.md
+++ react/components/General/EmptyContent/README.md
@@ -3,7 +3,7 @@
   title={'You don\'t have any product yet'}
   description={'Bring all your products here and show them to the world'}
   helpText={'Get help about products'}
-  handleHelp={() => console.log('--- handle help')}
+  helpLink={'http://www.google.com'}
   image={''}
 />
 ```
--- react/components/General/EmptyContent/index.js
+++ react/components/General/EmptyContent/index.js
@@ -2,17 +2,17 @@ import * as React from 'react'
 
 import IconQuestionCircle from '../../../icons/IconQuestionCircle/index'
 
-const EmptyContent = ({ title, description, helpText, image, handleHelp }) => {
+const EmptyContent = ({ title, description, helpText, image, helpLink }) => {
   return (
     <div className={'g-pa4 h-100 bg-white br2 ma7 flex flex-column items-center'}>
       <span className={'f3 fw6 mb4 mt9'}>{ title }</span>
       <span className={'f5 black-40 mb8'}>{ description }</span>
 
       {helpText && (
-        <div onClick={handleHelp} className={'pointer flex content-center black-40 link mb8'}>
+        <a href={helpLink} target={'_blank'} className={'pointer flex content-center black-40 link mb8'}>
           <IconQuestionCircle />
           <span className={'ml3'}>{ helpText }</span>
-        </div>
+        </a>
       )}
 
       <img src={image} />
