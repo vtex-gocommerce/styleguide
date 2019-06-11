50e6e8cd783d8912379e93d7146c024c04b3a83c
Gustavo Rosolem <guspace92@hotmail.com>
2019-06-11 17:20:39 -0300

    Show title in Card only when exists

--- react/components/Cards/Card/index.js
+++ react/components/Cards/Card/index.js
@@ -11,15 +11,15 @@ class Card extends Component {
 
     return (
       <div className={classes}>
-        <h3 className="flex items-center g-pt4 b--base-1 g-ma0 g-ph4 g-f1 fw6 c-on-base">{title}</h3>
+        {title && <h3 className="flex items-center g-pt4 b--base-1 g-ma0 g-ph4 g-f1 fw6 c-on-base">{title}</h3>}
         <PlaceholderContainer
           classNameArray={[
             'w-30 g-h6 g-ml4 g-mt5 br3',
             'w-60 g-h4 g-ml4 g-mt6 br2',
             'w-70 g-h4 g-ml4 g-mt3 br2',
-            'w-40 g-h4 g-ml4 g-mt3 br2 g-mb5'
+            'w-40 g-h4 g-ml4 g-mt3 br2 g-mb5',
           ]}
-          isPlaceholderActive={this.props.isLoading}
+          isPlaceholderActive={isLoading}
         >
           {() => (
             <div className={`g-pv4 g-ph4 flex flex-column flex-auto ${styles.cardContent}`}>{this.props.children}</div>
@@ -35,7 +35,7 @@ Card.propTypes = {
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
   /** Display a card with placeholders. */
   isLoading: PropTypes.bool,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 }
 
 export default Card
