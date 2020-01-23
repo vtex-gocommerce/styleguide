a77d3c6cb73141b043a66d3ed8393d62fa861eb4
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2020-01-23 12:06:21 -0300

    Show tooltip on children mosuehover/mouseout

--- react/components/Indicators/Tooltip/index.js
+++ react/components/Indicators/Tooltip/index.js
@@ -3,29 +3,41 @@ import PropTypes from 'prop-types'
 import styles from './style.css'
 
 class Tooltip extends Component {
+  state = {
+    show: false
+  }
   render() {
     return (
-      <div
-        className={`gc_tooltip z-5 absolute br2 g-pa4 bg-base-inverted-1 c-white
-        ${styles.gc_tooltip} ${this.props.className} ${!this.props.show ? 'dn' : ''}`}
-        style={{ width: this.props.width }}
-      >
-        {this.props.children}
-      </div>
+      <>
+        <div className="dib mr2 relative">
+          <div
+            className={`gc_tooltip z-5 absolute br2 g-pa2 bg-base-inverted-1 g-f1 c-white tc
+            ${styles.gc_tooltip} ${this.props.className} ${this.state.show ? '' : 'dn'}`}
+            style={{ width: this.props.width }}
+          >
+            {this.props.label}
+          </div>
+          <div
+            onMouseEnter={() => this.setState({ show: true })}
+            onMouseLeave={() => this.setState({ show: false })}
+          >
+            {this.props.children}
+          </div>
+        </div>
+      </>
     )
   }
 }
 
 Tooltip.propTypes = {
-  show: PropTypes.bool,
   width: PropTypes.string,
+  label: PropTypes.string,
   children: PropTypes.node.isRequired,
   className: PropTypes.string
 }
 
 Tooltip.defaultProps = {
-  show: false,
-  width: 'auto',
+  width: '200px',
   className: ''
 }
 
