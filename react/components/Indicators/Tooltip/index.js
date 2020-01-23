b55bb748040f5534f738c603a6c46640532c60d8
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2020-01-23 14:03:32 -0300

    Change to functional component

--- react/components/Indicators/Tooltip/index.js
+++ react/components/Indicators/Tooltip/index.js
@@ -1,44 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './style.css'
 
-class Tooltip extends Component {
-  state = {
-    show: false
-  }
-  render() {
-    return (
-      <>
-        <div className="dib mr2 relative">
-          <div
-            className={`gc_tooltip z-5 absolute br2 g-pa2 bg-base-inverted-1 g-f1 c-white tc
-            ${styles.gc_tooltip} ${this.props.className} ${this.state.show ? '' : 'dn'}`}
-            style={{ width: this.props.width }}
-          >
-            {this.props.label}
-          </div>
-          <div
-            onMouseEnter={() => this.setState({ show: true })}
-            onMouseLeave={() => this.setState({ show: false })}
-          >
-            {this.props.children}
-          </div>
-        </div>
-      </>
-    )
-  }
+const Tooltip = ({ className = '', children, text }) => {
+  const [show, setShow] = useState(false)
+
+  return (
+    <div className="dib mr2 relative">
+      <div
+        className={`gc_tooltip z-5 absolute br2 g-pa2 bg-base-inverted-1 g-f1 c-white tc
+          ${styles.gc_tooltip} ${className} ${show ? '' : 'dn'}`
+        }
+        style={{ width: '200px' }}
+      >
+        <span>{text}</span>
+      </div>
+      <div
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        {children}
+      </div>
+    </div>
+  )
 }
 
 Tooltip.propTypes = {
-  width: PropTypes.string,
-  label: PropTypes.string,
+  text: PropTypes.string,
   children: PropTypes.node.isRequired,
   className: PropTypes.string
-}
-
-Tooltip.defaultProps = {
-  width: '200px',
-  className: ''
 }
 
 export default Tooltip
