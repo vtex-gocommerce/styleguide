06e29288f8a72ef0382ec10e603295c5a44572e1
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2020-01-27 13:56:33 -0300

    Update TableTree props

--- react/components/Data/TableTree/index.js
+++ react/components/Data/TableTree/index.js
@@ -164,7 +164,7 @@ TableTree.propTypes = {
   ).isRequired,
   /** Rows that will be show on table. */
   rows: PropTypes.array.isRequired,
-  /** Optional data used on select change instead of rows. */
+  /** Mapped rows data returned on select change. */
   data: PropTypes.array,
   /** Makes rows selectable. */
   selectable: PropTypes.bool,
